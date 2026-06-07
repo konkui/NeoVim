0a6b0bba1d259cabeed2029b4ab51e2e0434719a
konkui <1308818014@qq.com>
2026-06-07 23:58:34 +0800

    sysnc

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,19 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = vim.api.nvim_create_augroup("auto_pull_config", { clear = true }),
+  callback = function()
+    local config_path = vim.fn.stdpath("config") -- ~/.config/nvim
+    vim.fn.jobstart({ "git", "-C", config_path, "pull", "--ff-only", "--quiet" }, {
+      detach = true,
+      on_exit = function(_, code)
+        if code == 0 then
+          vim.schedule(function()
+            vim.notify("Config updated", vim.log.levels.INFO)
+          end)
+        end
+      end,
+    })
+  end,
+})
